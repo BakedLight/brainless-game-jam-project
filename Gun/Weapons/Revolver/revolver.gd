21de28bd834fa0729e387bedffcae5d896dced48
SHIV <briyanishiv1234@gmail.com>
2026-07-08 16:20:49 +0530

    Weapons improvement
    Added fire rate, auto-shoot and switching between main and special weapon.

--- Gun/Weapons/Revolver/revolver.gd
+++ Gun/Weapons/Revolver/revolver.gd
@@ -1,10 +1,10 @@
 extends Polygon2D
 
-@export var fire_cooldown: float = 0.2
+signal cooldown_ended
 
-#@onready var fire_cooldown: Timer = $FireCooldown
+@onready var fire_cooldown: Timer = $FireCooldown
 
-var auto_shoot: bool = false
+@export var auto_shoot: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,9 +12,13 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 #
 #func _input(event: InputEvent) -> void:
 	#if Input.is_action_just_pressed("shoot_main"):
 		#fire_cooldown.start()
+
+
+func _on_fire_cooldown_timeout() -> void:
+	cooldown_ended.emit()
--- Player/player.gd
+++ Player/player.gd
@@ -10,17 +10,19 @@ var current_speed:int = 300
 
 var current_special:String = "knife"
 
-var in_hand: String = "gun"
+var can_shoot:bool = true
 
 var specials:Array = ["knife"]
 
 @onready var bullet_pos: Marker2D = $BulletPos
 @onready var weapon_switcher: AnimationPlayer = $Weapons/WeaponSwitcher
-@onready var revolver: Polygon2D = $Weapons/Revolver
+@onready var revolver: Polygon2D = $Weapons/Primary/Revolver
+@onready var primary: Node2D = $Weapons/Primary
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	look_at(direction)
+	primary.get_child(0).cooldown_ended.connect(can_shoot_now)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -28,6 +30,18 @@ func _process(_delta: float) -> void:
 	move_and_slide()
 	
 	look_at(get_global_mouse_position())
+	
+	if Input.is_action_pressed("shoot_main"):
+		if can_shoot:
+			if primary.get_child(0).auto_shoot:
+				shoot.emit("bullet", bullet_pos.global_position, Vector2((get_global_mouse_position().x - global_position.x), (get_global_mouse_position().y - global_position.y)).normalized())
+				can_shoot = false
+				primary.get_child(0).fire_cooldown.start()
+			else:
+				if Input.is_action_just_pressed("shoot_main"):
+					shoot.emit("bullet", bullet_pos.global_position, Vector2((get_global_mouse_position().x - global_position.x), (get_global_mouse_position().y - global_position.y)).normalized())
+					can_shoot = false
+					primary.get_child(0).fire_cooldown.start()
 
 func _input(_event: InputEvent) -> void:
 	direction = Vector2.ZERO
@@ -46,13 +60,13 @@ func _input(_event: InputEvent) -> void:
 	else:
 		current_speed = normal_speeed
 	 
-	
-	if Input.is_action_just_pressed("shoot_main"):
-		shoot.emit("bullet", bullet_pos.global_position, Vector2((get_global_mouse_position().x - global_position.x), (get_global_mouse_position().y - global_position.y)).normalized())
-		await get_tree().create_timer(revolver.fire_cooldown)
 	if Input.is_action_just_pressed("special"):
+		primary.get_child(0).fire_cooldown.stop()
+		can_shoot = false
 		weapon_switcher.play("fade_gun_to_special")
 
 func throw_special():
 	shoot.emit(current_special, bullet_pos.global_position, Vector2((get_global_mouse_position().x - global_position.x), (get_global_mouse_position().y - global_position.y)).normalized())
 	
+func can_shoot_now():
+	can_shoot = true
