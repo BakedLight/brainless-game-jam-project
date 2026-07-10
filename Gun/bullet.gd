8710926fda70a9c5b0e00961765ba570ef393bc3
SHIV <briyanishiv1234@gmail.com>
2026-07-10 19:37:05 +0530

    Made placeholder player sprite
    Also tweaked some projectile values

--- Gun/bullet.gd
+++ Gun/bullet.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var direction: Vector2 = Vector2.UP
-@export var speed: int = 1500
+@export var speed: int = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
